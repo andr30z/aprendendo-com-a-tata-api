3e60652d1660aacc4e91c11c571d6defe8372e4a
André Luiz Dias de Oliveira <51346021+andr30z@users.noreply.github.com>
2021-11-14 21:57:12 -0300

    Solving _id problem in activity controller endpoints

--- src/api/activities/entities/activity.entity.ts
+++ src/api/activities/entities/activity.entity.ts
@@ -1,5 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Transform } from 'class-transformer';
+import { classToPlain, Transform } from 'class-transformer';
 import { Document, ObjectId, Schema as MongooseSchema } from 'mongoose';
 import { DEFAULT_MONGOOSE_SCHEMA_OPTIONS } from 'src/database';
 import {
@@ -9,8 +9,31 @@ import {
 } from '../types';
 
 export type ActivityDocument = Activity & Document;
+// thank you stackoverflow user
+// i should change this to an utils folder...
+function iter(o: any) {
+  Object.keys(o).forEach(function (k) {
+    if (k === '_id') {
+      o[k] = o[k].toString();
+      return;
+    }
+    if (o[k] !== null && typeof o[k] === 'object') return iter(o[k]);
+    if (Array.isArray(o[k])) return o[k].map(iter);
+  });
+}
 
-@Schema({ ...DEFAULT_MONGOOSE_SCHEMA_OPTIONS, strict: false })
+@Schema({
+  ...DEFAULT_MONGOOSE_SCHEMA_OPTIONS,
+  toJSON: {
+    transform: (_, ret) => {
+      let returnObj = { ...ret, _id: ret._id.toString() };
+      //this is a popular case of the famous ""JEITINHO BRASILEIRO"
+      iter(returnObj);
+      return classToPlain(returnObj);
+    },
+  },
+  strict: false,
+})
 export class Activity {
   @Transform(({ obj }) => obj._id.toString())
   _id: ObjectId;
@@ -34,7 +57,7 @@ export class Activity {
   dificulty: ActivityDificultyTypes;
 
   @Prop({ required: true, type: MongooseSchema.Types.Mixed })
-  stages: any;
+  stages: Array<any>;
 
   @Prop()
   avaliationMethod: AvaliationMethods;
--- src/database/utils/default-mongoose-schema-options.util.ts
+++ src/database/utils/default-mongoose-schema-options.util.ts
@@ -5,6 +5,7 @@ export const DEFAULT_MONGOOSE_SCHEMA_OPTIONS: SchemaOptions = {
   timestamps: true,
   toJSON: {
     transform: (_doc: any, ret: any) => {
+      console.log(ret);
       // THIS IS BEIGN USED BECAUSE NESTJS/MONGOOSE/CLASS-VALIDATOR SERIALIZATION SUCKS, E.G: SOME TIMES THE _id FIELD WOULD NEVER SERIALIZE
       // THEN ON THE RESPONSE IT WOULD COME IN SHAPE OF AN EMPTY OBJECT.
       return classToPlain({ ...ret, _id: ret._id.toString() });
@@ -16,7 +17,7 @@ export const getDefaultSchemaOption = (
   customSchemaOptions: SchemaOptions = {},
 ) => ({ ...DEFAULT_MONGOOSE_SCHEMA_OPTIONS, ...customSchemaOptions });
 
-//AVERAGE toJSON ENJOYER:
+//REGULAR toJSON ENJOYER:
 //  ⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠛⠛⠛⠋⠉⠈⠉⠉⠉⠉⠛⠻⢿⣿⣿⣿⣿⣿⣿⣿
 //  ⣿⣿⣿⣿⣿⡿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⢿⣿⣿⣿⣿
 //  ⣿⣿⣿⣿⡏⣀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣤⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿
@@ -40,7 +41,7 @@ export const getDefaultSchemaOption = (
 //  ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠙⣿⣿⡟⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿
 //  ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠁⠀⠀⠹⣿⠃⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿
 
-//REGULAR ClassSerializerInterceptor FAN
+//AVERAGE ClassSerializerInterceptor FAN
 // ⣿⣿⠟⠁⠄⢤⣰⣶⣕⡝⠷⠙⠻⢨⡁⣨⣙⠳⢈⣉⡛⠿⡟⠸⠸⡟⣇⢻⣿
 // ⣿⠏⠄⠄⢴⣿⣿⠮⢫⣴⣾⣿⣿⣿⣿⣿⣿⣷⣾⣿⠿⢶⣴⣷⣆⡁⠇⢸⣿
 // ⠇⠄⢘⣻⣿⣛⠊⣰⣿⣿⣿⣿⣿⣿⡿⢟⠯⠭⠿⣛⡻⢶⡿⣿⣿⣧⠄⣼⣿
